fe8d765d4fcc4532913134cb6548c90ed1bcfb9d
Andrea.Parrini.Lavoro <andrea.parrini26@gmail.com>
2025-03-14 09:50:04 +0100

    add a new route
    add a other method to create a route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, /* createRoutesFromElements, Route */ } from "react-router-dom";
 import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+
+/* ALTRO MODO PER DEFINIRE LE ROTTE
+Se si usa questo modo vanno importati anche {createRoutesFromElements, Route} from "react-router-dom";
+
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/products" element={<ProductsPage />} />
+  </Route>
+)
+
+const router = createBrowserRouter(routeDefinitions); */
 
 const router = createBrowserRouter([
-  {path: '/', element: <HomePage /> }
+  {path: '/', element: <HomePage /> },
+  {path: '/products', element: <ProductsPage /> },
+
 ])
 
 function App() {
