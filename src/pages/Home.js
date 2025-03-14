da54f5f463234d47a4c61fb37f1299134fd9b5d1
Andrea.Parrini.Lavoro <andrea.parrini26@gmail.com>
2025-03-14 11:04:43 +0100

    add useNavigate

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,24 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function HomePage(){
+
+    /* useNavigate serve per navigare in modo programmatico e forza la navigazione dell'utente verso un'altra rotta
+        per esempio usato alla fine di un timer per reindirizzare su un'altra pagina  */
+    
+    const navigate= useNavigate();
+
+    function navigateHandler(){
+        navigate('/products')
+    }
+
     return (
     <>
         <h1>My Home Page</h1>
         <p>
             Go to <Link to="/products">the list of products</Link>.
+        </p>
+        <p>
+            <button onClick={navigateHandler}>Navigate</button>
         </p>
     </>
     )
