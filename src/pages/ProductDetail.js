39b5b182eb696c5e72a464e3ff4703671b905a7f
Andrea.Parrini.Lavoro <andrea.parrini26@gmail.com>
2025-03-14 11:21:38 +0100

    modify a type of paths
    add all relative path in the all link

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 function ProductDetailPage(){
 
@@ -9,6 +9,15 @@ function ProductDetailPage(){
     <p>
         {params.productId}
     </p>
+    <p>
+        <Link to='..' relative='path'>Back</Link> 
+        {/* 
+        Per default il link ha come prop in relative il valore route, in questo modo gli stiamo dicendo di tornnare indietro di un livello nel percorso 
+        E quindi passera da /products/p1 a /products
+        Se lasciavamo route tornava alla rotta precedente che era semplicemente / e quindi tornava in home page.
+        */}
+    </p>
+    
     </>
 }
 
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -11,7 +11,7 @@ function ProductsPage(){
     <>
         <h1>The products page</h1>
         <ul>
-            {PRODUCTS.map(prod => <li key={prod.id}><Link to={`/products/${prod.id}`}>{prod.title}</Link></li>)}
+            {PRODUCTS.map(prod => <li key={prod.id}><Link to={prod.id}>{prod.title}</Link></li>)}
         </ul>
     </>
     
