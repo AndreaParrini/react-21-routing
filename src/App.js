929fc0402501a6aabb7f8063ad9370309915579b
Andrea.Parrini.Lavoro <andrea.parrini26@gmail.com>
2025-03-14 10:18:17 +0100

    create a root layout to view in all page
    use property children and then in the components use outlet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider, /* createRoutesFromElements, Route */ } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
 
 /* ALTRO MODO PER DEFINIRE LE ROTTE
 Se si usa questo modo vanno importati anche {createRoutesFromElements, Route} from "react-router-dom";
@@ -15,8 +16,14 @@ const routeDefinitions = createRoutesFromElements(
 const router = createBrowserRouter(routeDefinitions); */
 
 const router = createBrowserRouter([
-  {path: '/', element: <HomePage /> },
-  {path: '/products', element: <ProductsPage /> },
+  {
+    path:'/', 
+    element: <RootLayout />,
+    children: [
+      {path: '/', element: <HomePage /> },
+      {path: '/products', element: <ProductsPage /> },
+    ]},
+  
 
 ])
 
